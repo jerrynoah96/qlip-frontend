eb1081115113fa02c03b6f17ea867398088b1d5c
Adek <ajayimikeofficial@gmail.com>
2021-09-23 06:08:36 +0100

    'choose create' page now responsive

--- src/components/chooseCreate.js
+++ src/components/chooseCreate.js
@@ -15,40 +15,37 @@ const ChooseCreate = (props)=> {
 
     return(
         <div className="choose-create">
-            <div className="choose-create-heading">
-            <div className="choose-describe">
-            <h1>Create your QLIP Collectibles</h1>
-            
-            Choose <p>“Single”</p>  if you want your collectible to 
-            be one of a kind or <p>“Multiple”</p> if you want to sell one collectible multiple times
-            </div>
-
-            </div>
-
-            <div className="create-options">
-                <div className="create-option">
-                    <img src={singleLogo} alt="single"/>
-
-                    <h2 className="choice-title">Single</h2>
-                    <span className="choice-desc"> create a one kind collectible</span>
-
-                    <button
-                    onClick={setPage}>Create Single</button>
+            <div className = "choose-create-container">
+                <div className="choose-create-heading">
+                    <h1>Create your QLIP Collectibles</h1>
+                    <p>Choose <span>“Single”</span>  if you want your collectible to be one of a kind or <span>“Multiple”</span> if you want to sell one collectible multiple times</p>
                 </div>
 
-                <div className="create-option create-multiple">
-                    <img src={multipleLogo} alt="multiple"/>
+                <div className="create-options">
+                    <div className="create-option">
+                        <div className = "image-container">
+                            <img src={singleLogo} alt="single"/>
+                        </div>
+                        <div className = "option-text">
+                            <h2 className="choice-title">Single</h2>
+                            <span className="choice-desc"> create a one kind collectible</span>
+                        </div>
+                        <button onClick={setPage}>Create Single</button>
+                    </div>
 
-                    <h2 className="choice-title">Multiple</h2>
-                    <span className="choice-desc"> sell one collectible multiple times</span>
+                    <div className="create-option create-multiple">
+                        <div className = "image-container">
+                            <img src={multipleLogo} alt="multiple"/>
+                        </div>
+                        <div className = "option-text">
+                            <h2 className="choice-title">Multiple</h2>
+                            <span className="choice-desc"> sell one collectible multiple times</span>
+                        </div>
+                        <button type="button" disabled>Create Multiple</button>
+                    </div>
 
-                    <button
-                    type="button" disabled>Create Multiple</button>
                 </div>
-
-            </div>
-
-           
+            </div>     
             
         </div>
 
