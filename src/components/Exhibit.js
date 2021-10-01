19f5c65d0db45ba95509cb90bfef9f2954f001c4
Adek <ajayimikeofficial@gmail.com>
2021-10-01 16:09:04 +0100

    update exhibit

--- src/components/Exhibit.js
+++ src/components/Exhibit.js
@@ -4,6 +4,8 @@ import verifiedIcon from "../images/icons8_verified_account.svg"
 import CheckoutModal from "./CheckoutModal"
 import {useParams} from 'react-router-dom'
 import { useHistory } from "react-router-dom";
+import Web3 from "web3";
+import contractABI from '../contractAbi.json';
 
 import "../styles/exhibit.css"
 
@@ -30,21 +32,114 @@ const Exhibit = (props) => {
         }
     }
 
+    let web3 = new Web3(Web3.givenProvider)
+    const contractAddress = "0x178315F9Dc7D0666D8c28F2a23644e34eD44c57D";
+    let contractInstance;
+
+
+    const initWeb3 = async tokenID => {
+
+        web3 = new Web3(window.ethereum);
+
+        contractInstance = new web3.eth.Contract(contractABI, contractAddress);
+
+        const tokenDetails = await contractInstance.methods.getAllTokenDetails(tokenID).call();
+
+        if(tokenDetails.ownerAddress) {
+
+            const salePrice = await contractInstance.methods.getSalePrice(Number(tokenDetails._id)).call();
+            const TokenState = await contractInstance.methods.getNFTState(Number(tokenDetails._id)).call();
+
+            fetch(tokenDetails.tokenURI_).then(res => res.json()).then(data => {
+
+                setPageData({
+                    id: tokenDetails._id,
+                    item_name: data.item_name,
+                    imgUrl: data.imgHash,
+                    description: data.description,
+                    price: Web3.utils.fromWei(salePrice, "ether"),
+                    owner: tokenDetails.tokenOwner,
+                    state: TokenState
+                })
+
+                console.log(data);
+            })
+            
+            
+        }
+
+        
+
+    }
+
+    const initWeb3Fallback = async tokenID => {
+        web3 = new Web3('https://data-seed-prebsc-1-s1.binance.org:8545/');
+
+        contractInstance = new web3.eth.Contract(contractABI, contractAddress);
+
+        const tokenDetails = await contractInstance.methods.getAllTokenDetails(tokenID).call();
+
+        if(tokenDetails.ownerAddress) {
+
+            const salePrice = await contractInstance.methods.getSalePrice(Number(tokenDetails._id)).call();
+            const TokenState = await contractInstance.methods.getNFTState(Number(tokenDetails._id)).call();
+
+            fetch(tokenDetails.tokenURI_).then(res => res.json()).then(data => {
+
+                setPageData({
+                    id: tokenDetails._id,
+                    item_name: data.item_name,
+                    imgUrl: data.imgHash,
+                    description: data.description,
+                    price: Web3.utils.fromWei(salePrice, "ether"),
+                    owner: tokenDetails.tokenOwner,
+                    state: TokenState
+                })
+
+                console.log(data);
+            })
+            
+            
+        }
+    }
+
+        
+    
+
+
+
     useEffect(() => {
+
         if(!tokenId) return history.replace("/");
 
-        const currentNFT = props.allTokensArray.find(token => token.id === tokenId);
 
-        if(!currentNFT) return history.replace("/");
+        if(window.ethereum || window.web3) {
+            
+            const chainId = window.ethereum.chainId;
+            
+            if (chainId === "0x61") {
+                initWeb3(Number(tokenId))
+            } else {
+                initWeb3Fallback(Number(tokenId))
+            }
+           
+
+        } else {
+            // if the user is using non-ethereum enabled browser
+
+            initWeb3Fallback(Number(tokenId))
+        }
+
+        // if(!currentNFT) return history.replace("/");
         
-        setPageData(currentNFT)
+        // setPageData(currentNFT)
         
 
     }, [])
     return(
         
         <>
-            <div className = "exhibit-main-body">
+            {!pageData.id ? <p>LOADING...</p> : <div className = "exhibit-main-body">
                 <div className = "nft-picture-container exhibitPic">
                     <img src = {pageData.imgUrl} class = "nft-picture" alt = "nft" />
                     <div className = "nft-description">
@@ -98,6 +193,7 @@ const Exhibit = (props) => {
                     </div>
                 </div>
             </div>
+            }
             <CheckoutModal ref = {checkoutModalRef} closeModal = {toggleModal} tokenDetails={pageData} 
             contractDetails = {props.contractDetails}
             web3={props.web3}/>
